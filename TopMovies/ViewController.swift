377453f8de482ee8077e521f5b7866bdcec9d4a9
Daniel Sanchez <daniel.sanchez@qacg.com>
2024-10-22 11:56:19 -0600

    Initial Commit

---
+++ TopMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TopMovies
+//
+//  Created by Iván Sánchez Torres on 22/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TopMoviesTests/TopMoviesTests.swift
@@ -0,0 +1,17 @@
+//
+//  TopMoviesTests.swift
+//  TopMoviesTests
+//
+//  Created by Iván Sánchez Torres on 22/10/24.
+//
+
+import Testing
+@testable import TopMovies
+
+struct TopMoviesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
